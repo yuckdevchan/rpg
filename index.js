c965d59ca9d7ed9be15dda9898b32f1459c26a49
manSlayer16 <120093141+manSlayer16@users.noreply.github.com>
2023-04-18 14:38:49 +0100

    added the link to the enemy health id

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 att_btn = document.getElementById("attack_btn")
-
-EnemyHealth = 0
+EnemyHealth = documen.getElementById("EnemyHealth")
+EHealth = 0
 Attack = 0
 
 function attack() {
-  EnemyHealth = EnemyHealth - Attack
+  EHealth = EnemyHealth - Attack
   att_btn.InnerText = EnemyHeath
 }
